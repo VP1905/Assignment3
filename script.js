cc6305bd967e06772bd837b5df02343d347b8910
VP1905 <Vpatel0589@conestogac.on.ca>
2025-11-19 19:36:15 -0500

    Updated frontend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://assignment3-frontend-g9i6.onrender.com";
 
 const API_PRODUCTS = `${API_BASE_URL}/products`;
 const API_CART = `${API_BASE_URL}/cart_items`;
